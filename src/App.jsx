dcd5c577754e7cc8a57bdab3622a1f7dbe9a8928
Sameer <rasengankid999@gmail.com>
2022-04-15 23:17:16 +0545

    Components and Extra Configurations

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+/*
+function App() {
+  return (
+        <>
+          <Header/>
+          <Footer/>
+          <Main/>
+        </>
+  );
+}
+*/
+
+
+
+function App() {
+  return (
+        <>
+        
+         <h1>Hello World!</h1>
+        </>
+  );
+}
+
+export default App;
