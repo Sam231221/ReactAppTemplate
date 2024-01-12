74ab4bf37e0796f2536942034581a1f65177ff4f
Sameer Shahi Thakuri (Sam) <70689865+Sam231221@users.noreply.github.com>
2024-01-12 10:03:59 +0545

    crud demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
+import { ErrorPage } from "./utilities/404Error";
+import { Home } from "./pages/Home";
+import { AddUser } from "./components/Users/AddUser";
+import { UpdateUser } from "./components/Users/UpdateUser";
+import ViewUser from "./components/Users/ViewUser";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path={"/"} element={<Home />}></Route>
+          <Route exact path={"/users/add/"} element={<AddUser />} />
+          <Route exact path={"/users/update/:id/"} element={<UpdateUser />} />
+          <Route exact path={"/users/:id/"} element={<ViewUser />} />
+          <Route element={<ErrorPage />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
